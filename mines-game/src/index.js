4a1d85be1c9353949503fa734df5d5fe33d104df
Eric <ericc404@nycstudents.net>
2024-12-18 00:07:32 -0500

    Basic UI generation

---
+++ mines-game/src/index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const DOMselectors = {
+    gameContainer: document.querySelector(".game-container"),
+    gameBoard: document.querySelector(".game-board"),
+    gameQuote: document.querySelector(".game-quote"),
+  };
+
+  function createButton() {
+    const button = document.createElement("button");
+    button.className =
+      "w-16 h-16 bg-gray-700 hover:bg-gray-500 border border-gray-800 rounded-md shadow-md";
+    button.textContent = "";
+    return button;
+  }
+
+  for (let i = 0; i < 5 * 5; i++) {
+    const button = createButton();
+    DOMselectors.gameBoard.appendChild(button);
+  }
+});
